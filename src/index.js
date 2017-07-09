2b1acccb5644e45d6ce6c0cb441f8705f4988011
kumavis <aaron@kumavis.me>
2017-07-09 14:27:56 -0700

    resolver - add isLink

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ exports.util = {
 exports.resolver = {
   multicodec: trieIpldFormat,
   tree: tree,
-  resolve: resolve
+  resolve: resolve,
+  isLink: isLink
 }
 
 function resolve (block, path, callback) {
@@ -37,5 +38,23 @@ function tree (block, options, callback) {
       if (err) return callback(err)
       callback(null, result)
     })
+  })
+}
+
+function isLink (block, path, callback) {
+  resolve(block, path, (err, result) => {
+    if (err) {
+      return callback(err)
+    }
+
+    if (result.remainderPath.length > 0) {
+      return callback(new Error('path out of scope'))
+    }
+
+    if (typeof result.value === 'object' && result.value['/']) {
+      callback(null, result.value)
+    } else {
+      callback(null, false)
+    }
   })
 }
